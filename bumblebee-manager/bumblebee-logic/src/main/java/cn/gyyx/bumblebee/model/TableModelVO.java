ef3eca02beb0f1b5a942c822438ffa102b818834
eastFu <492510931@qq.com>
2017-09-25 09:30:27 +0800

    fix operation log node

--- bumblebee-manager/bumblebee-logic/src/main/java/cn/gyyx/bumblebee/model/TableModelVO.java
+++ bumblebee-manager/bumblebee-logic/src/main/java/cn/gyyx/bumblebee/model/TableModelVO.java
@@ -1,5 +1,7 @@
 package cn.gyyx.bumblebee.model;
 
+import java.util.Map;
+
 /**
  * @Author : east.Fu
  * @Description :
@@ -7,12 +9,31 @@ package cn.gyyx.bumblebee.model;
  */
 public class TableModelVO {
 
+    /**
+     * 刷新的次数
+     */
     private Integer draw;
+
+    /**
+     * 开始序号index
+     */
     private Integer start;
+
+    /**
+     * pageSize
+     */
     private Integer length;
+
+    /**
+     * search[value]
+     * search[regex]   true/false
+     */
+    private Map<String,String> search;
+
     public Integer getDraw() {
         return draw;
     }
+
     public void setDraw(Integer draw) {
         this.draw = draw;
     }
@@ -29,4 +50,11 @@ public class TableModelVO {
         this.length = length;
     }
 
+    public Map<String, String> getSearch() {
+        return search;
+    }
+
+    public void setSearch(Map<String, String> search) {
+        this.search = search;
+    }
 }
